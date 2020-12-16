6ef1e9d052586380098f207801ea985f5c535c95
NatroNx <NatroNx@users.noreply.github.com>
2020-12-16 23:54:55 +0100

    Update IONIQ_BEV.py

--- cars/IONIQ_BEV.py
+++ cars/IONIQ_BEV.py
@@ -3,17 +3,27 @@ class IONIQ_BEV:
     def __init__(self, dongle):
         self.dongle = dongle
         self.dongle.setProtocol('CAN_11_500')
-        self.dongle.setCANRxFilter('7EC')
         self.dongle.setCANRxMask('7FF')
 
     def getData(self):
         raw = {}
-
+		
+        self.dongle.setCANTx('7E4')		
+        self.dongle.setCANRxFilter('7EC')
         for cmd in [2101,2105]:
             raw[cmd] = self.dongle.sendCommand(str(cmd))
-
+ 
+        self.dongle.setCANTx('7e6')	
+        self.dongle.setCANRxFilter('7EE')
+        raw[2180] = self.dongle.sendCommand(str(2180))
+        raw[2202] = {0x7CE22: 'noData'} #initdata  - will fail at filter if charging
         chargingBits = raw[2101][0x7EC21][5] \
                 if 0x7EC21 in raw[2101] else None
+        if chargingBits != None and chargingBits & 0x80 != 0x80:
+            self.dongle.setCANTx('7c6')		
+            self.dongle.setCANRxFilter('7CE')
+            raw[2202] = self.dongle.sendCommand('22b002')
+		   
         dcBatteryCurrent = int.from_bytes(raw[2101][0x7EC21][6:7] + raw[2101][0x7EC22][0:1],
                 byteorder='big', signed=True) / 10.0 \
                 if 0x7EC21 in raw[2101] and 0x7EC22 in raw[2101] else None
@@ -26,6 +36,9 @@ class IONIQ_BEV:
                 'SOC_DISPLAY':  raw[2105][0x7EC24][6] / 2.0 \
                     if 0x7EC24 in raw[2105] else None,
                 'EXTENDED': {
+				    'externalTemperature':    (raw[2180][0x7EE22][1] - 80) / 2.0 \
+					    if 0x7EE22 in raw[2180] else None,
+						
                     'auxBatteryVoltage':        raw[2101][0x7EC24][4] / 10.0 \
                         if 0x7EC24 in raw[2105] else None,
 
@@ -40,6 +53,18 @@ class IONIQ_BEV:
                     'batteryMinTemperature':    int.from_bytes(raw[2101][0x7EC22][4:5],
                         byteorder='big', signed=True) \
                         if 0x7EC22 in raw[2105] else None,
+
+                    'cumulativeEnergyCharged':  int.from_bytes(raw[2101][0x7EC25][6:7] + raw[2101][0x7EC26][0:3],
+                        byteorder='big', signed=False) / 10 \
+                        if 0x7EC25 in raw[2101] else None,
+
+                    'cumulativeEnergyDischarged':  int.from_bytes(raw[2101][0x7EC26][3:7],
+                        byteorder='big', signed=False) / 10 \
+                        if 0x7EC26 in raw[2101] else None,
+
+                    'odo':  int.from_bytes(raw[2202][0x7CE21][3:6],
+                        byteorder='big', signed=False) \
+                        if 0x7CE21 in raw[2202] else None,
 
                     'charging':                 1 if chargingBits != None and \
                             chargingBits & 0x80 == 0x80 else 0,
